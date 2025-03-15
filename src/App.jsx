80344b2f2dc3416f0cd30a672100dbb7edf0c443
Anusha Dhakal <anushadhakal10@gmail.com>
2025-03-15 08:21:58 +0545

    app.css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+import Home from './Pages/Home'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
+
+
+
 function App() {
 
   return (
     <>
-      
+
+     <Home/>
+
+      <Navbar />
+      <Footer />
+
     </>
   )
 }
