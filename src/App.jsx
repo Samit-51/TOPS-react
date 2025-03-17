74563c1cef386b5eaa8922cd6f7baa45ce255d69
Samit-51 <devilgamingbrawlstars@gmail.com>
2025-03-17 20:26:04 +0545

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Academics from "./Pages/Academics";
+import Academics from "./Pages/Academics/Academics";
 import './App.css'
-import Contact from './Pages/Contact'
+import Contact from './Pages/Contact/Contact'
 import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
-import Events from './Pages/Events'
-import OurServices from "./Pages/OurServices";
+import Events from './Pages/Events/Events'
+import OurServices from "./Pages/Our services/OurServices";
 import Gallery from "./Pages/Gallery/Gallery";
 
 function App() {
